10055469a6d6fe7f9690894e3b57548cf515a340
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-10-09 16:55:21 +0200

    Add
    other style

--- script.php
+++ script.php
@@ -1,40 +1,37 @@
 <?php
 
+$paragraph = $_POST["paragraph"];
 
-$paragraph = $_POST["paragrafo"];
+$paragraph_length = strlen($paragraph);
 
-$paragraphLength = strlen($paragraph);
+$badword = $_POST['badword'];
 
-$badword = $_POST["badword"];
-
-var_dump($paragraphLength);
-
-
-$censuredWord = str_replace($badword, "******", $paragraph);
-
+$censured_text = str_replace($badword, '*****', $paragraph);
 
 
 ?>
 
-
-<!-- HTML -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>PHP Badwords</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+
 <body>
 
-<h1>Testo Originale:</h1>
-<p><?php echo $paragraph; ?></p>
-<h4><?php  "The text length is: $paragraphLength";  ?></h4>
-<h2>Testo Censurato:</h2>
-<p><?php echo $censuredWord; ?></p>
+    <h3>Testo originale : <?php echo $paragraph ?></h3>
+    <h3>Lunghezza testo: <?php echo $paragraph_length ?> caratteri</h3>
+    <h3>Parola Censurata:  <?php echo $badword ?></h3>
 
-<a href="index.php"> Back</a>
-    
+    <h3>testo censurato : <?php echo $censured_text ?></h3>
+
+    <a href="index.php">Back</a>
+
 </body>
+
 </html>
